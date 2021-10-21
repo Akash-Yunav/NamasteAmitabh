17593990d340610cce4114adff3dc14a88dac881
Akash <aakashsrivastava62@gmail.com>
2021-10-21 21:10:39 +0530

    Default commit

---
+++ script.js
@@ -0,0 +1,16 @@
+/* This is an event*/ 
+// alert("i'm Annoying");
+// var lastname = "Srivastava";
+// var fullname = firstname + " " + lastname;
+
+// var a = 1;
+
+// // document.write(username);
+// document.write("<h1>" + fullname + "</h1>");
+// // document.write(username);
+// document.write("<h1>" + fullname + "</h1>");
+// document.write("<h1>Hello, Akash!</h1>");
+
+var username = prompt("Apka naam kyaa hai!hain");
+
+document.write("<h1>Namaste, " + username + "!</h1>");
